3821584d06fd86418f9c7af17b7ea64c0bc1e06e
Rob Gietema <rob@fourdigits.nl>
2012-08-29 14:30:03 +0000

    Fix git import.

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='plonenl.plonetheme',
+      version=version,
+      description="Plone.nl Plone Theme",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='web zope plone theme',
+      author='Plone.nl',
+      author_email='support@fourdigits.nl',
+      url='http://plone.nl',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plonenl'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+      )
