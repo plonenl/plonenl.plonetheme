b3dd6218451cda1d5fa90cf8167e9e15398e6d2e
Franklin Kingma <franklin@fourdigits.nl>
2012-09-28 18:48:44 +0200

    Added collective.ploneslimbar

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name='plonenl.plonetheme',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'collective.ploneslimbar',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
